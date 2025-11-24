b344c57a6859120966eefe04b8c33511872190b0
welfet01 <welfet01@ucm.es>
2025-11-24 21:45:08 +0100

    main.cpp
    he hecho el main del proyecto

--- ejercicio_calculadora/main.cpp
+++ ejercicio_calculadora/main.cpp
@@ -1,11 +1,34 @@
-#include <iostream>
+﻿#include <iostream>
 #include "operations.h"
-
-
+#include <string>
 
 int main() {
+    mensajeInicio();
 
+    bool salir = false;
 
+    while (!salir) {
 
-	return 0;
+        tDatos d = lectura();
+
+        if (d.operador == "Exit") {
+            salir = true;
+        }
+        else if (d.operador == "Help") {
+            help();
+        }
+        else {
+            double r = execute(d);
+
+            
+            if (r == -1) {
+                mostrarError("Operacion invalida");
+            }
+            else {
+                mostrarSolucion(r);
+            }
+        }
+    }
+
+    return 0;
 }
