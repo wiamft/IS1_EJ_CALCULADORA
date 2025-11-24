72a04c04d7f0eb1f3c7d7ca237430f7ff8688234
wiamft <welfettahy@gmail.com>
2025-11-24 21:27:46 +0100

    operations.cpp
    he actualizado la parte de operations.cpp

--- ejercicio_calculadora/operations.cpp
+++ ejercicio_calculadora/operations.cpp
@@ -1,8 +1,41 @@
 #include "operations.h"
+#include "inout.h"
+#include <cmath>
 
-double add(double num1, double num2);
-double sub(double num1, double num2);
-double mul(double num1, double num2);
-double div(double num1, double num2);
-double exp(double num1, double num2);
-double execute(tDatos datos);
+
+double add(double num1, double num2) {
+    return num1 + num2;
+}
+double sub(double num1, double num2) {
+    return num1 - num2;
+}
+double mul(double num1, double num2) {
+    return num1 * num2;
+}
+double div(double num1, double num2) {
+    double resultado = -1;
+    if (num2 != 0) {
+        resultado = num1 / num2;
+    }
+    return resultado;
+}
+double exp(double num1, double num2) {
+
+    return pow(num1, num2);
+}
+
+double execute(tDatos datos) {
+    double resultado;
+    if (datos.operador == "Add")
+        resultado = add(datos.num1, datos.num2);
+    else if (datos.operador == "Sub")
+        resultado = sub(datos.num1, datos.num2);
+    else if (datos.operador == "Mul")
+        resultado = mul(datos.num1, datos.num2);
+    else if (datos.operador == "Div")
+        resultado = div(datos.num1, datos.num2);
+    else if (datos.operador == "Exp")
+        resultado = exp(datos.num1, datos.num2);
+    return resultado;
+}
+
